dfb612d2c3e0cde780278afb0ac445ba4eeaebb6
skipancho <ctg1770a@gmail.com>
2023-09-01 23:58:29 +0900

    [CHORE] #0 - 안드로이드 프로젝트 생성

---
+++ app/src/main/java/com/pet_platform/pet_k_in/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pet_platform.pet_k_in
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
